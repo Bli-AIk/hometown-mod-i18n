224206226c3b3c71a3805a9fb98a05e08f4b9010
fluffyboy <sekalisukarumah@gmail.com>
2026-05-29 17:31:16 +1000

    Updated slash.lua

--- mods/after_chill/scripts/battle/waves/shadow/slash.lua
+++ mods/after_chill/scripts/battle/waves/shadow/slash.lua
@@ -3,18 +3,16 @@ local slash, super = Class(Wave)
 function slash:init()
     super.init(self)
     self:setArenaPosition(320, 230)
-    self.time = 9
+    self.time = 13
 end
 
 function slash:onStart()
     local shadow = self:getAttackers()[1]
-    self.timer:everyInstant(1, function()
-    Assets.playSound("laz_c")
-    shadow:setAnimation("battle/attack", function()
-        shadow:shake(2)
-        shadow:setAnimation("battle/idle")
+    self.timer:everyInstant(1.2, function()
+    shadow:setAnimation("battle/act")
+    self.timer:after(0.1, function()
+        self:alertSlash()
     end)
-    self:alertSlash()
     end)
 end  
 
@@ -22,6 +20,13 @@ function slash:hitSlash(x, y)
     local sx, sy = x, y
     local bullet = self:spawnBullet("effects/attack/shard", sx, sy)
     bullet.destroy_on_hit = false
+    local shadow = self:getAttackers()[1]
+    Assets.playSound("laz_c", 0.4, 0.8)
+    Assets.playSound("scytheburst", 1.5)
+    shadow:setAnimation("battle/attack", function()
+        shadow:shake(2)
+        shadow:resetSprite()
+    end)
     bullet.sprite:play(0.15, false, function()
           bullet:remove()
     end)
@@ -29,18 +34,23 @@ end
 
 function slash:alertSlash()
     local x, y = self:getSoulCoords()
-    local sprite = Sprite("effects/attack/shard_1", x, y)
+    local sprite = Sprite("effects/shard", x, y)
     Game.stage:addChild(sprite) 
-    sprite.alpha = 0
+    sprite.alpha = 0.5
     sprite:setScale(2)
     sprite:setOrigin(0.5, 0.5)
-    sprite:addFX(ColorMaskFX(COLORS.red, 1)) 
     Assets.playSound("alert")
+    for i = 1, 3 do 
     self.timer:tween(0.1, sprite, {alpha = 0.5}, "out-expo", function()
     self.timer:tween(0.1, sprite, {alpha = 0}, "in-expo", function()
-      self:hitSlash(x, y)
+        if i == 2 then
+        self.timer:after(0.08, function()
+        self:hitSlash(x, y)
+        end)
+        end 
     end)
     end)
+end
 end 
 
 
