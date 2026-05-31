9dca86a11cf4fce735e539c6e10c8cb27374d929
DragonZenore <163686699+DragonZenore@users.noreply.github.com>
2026-05-31 18:40:41 -0400

    Add files via upload

--- mods/after_chill/scripts/data/actors/kris.lua
+++ mods/after_chill/scripts/data/actors/kris.lua
@@ -1,13 +1,10 @@
-local actor, super = Class("noelle", true)
+local actor, super = Class("kris", true)
 
 function actor:init()
     super.init(self)
     self.portrait_offset = {-20, -10}
     -- Table of sprite animations
     TableUtils.merge(self.animations, {
-        ["fall"]         = {"dark", 1/6, true},
-        ["float"]        = {"battle_alt/float", 1/8, true},
-        ["pray"]         = {"battle_alt/pray", 1/6, true},
         ["sweep"]        = {"battle/sweep", 1/4, true}
     }, false)
 end
