edbaf8c30ce67acb37342ca7317288b746bd21a3
fluffyboy <sekalisukarumah@gmail.com>
2026-06-01 13:49:00 +1000

    dusteer fixing

--- mods/after_chill/scripts/battle/enemies/dusteer.lua
+++ mods/after_chill/scripts/battle/enemies/dusteer.lua
@@ -4,7 +4,7 @@ function dusteer:init()
     super.init(self)
 
     self.name = "Dusteer"
-    self:setActor("Dusteer")
+    self:setActor("dusteer")
 
     self.max_health = 340
     self.health = 340
@@ -30,95 +30,39 @@ function dusteer:init()
     self.low_health_text = "* You can see Peonie wilting slowly."
 
     self:registerAct("Sweep", "Get\nMercy")
-    self:registerXAct("N-Sweep", "Get\nMercy")
+    -- Game.battle:registerXAction("N-Sweep", "Get\nMercy")
 end
 
 function dusteer:onAct(battler, name)
-    if name == "Sweep" then
-        
+    if name == "Sweep" then    
         Game.battle:startActCutscene(function(cutscene)
-            set.animation = "sweep"
-            local kris = Game.battle:getPartyBattler("kris")
-            local x, y = Game.battle.enemies[1].x, Game.battle.enemies[1].y
-            kris:slideTo(x, y, 0.5)
-            kris:setAnimation("sweep")
+            self:setSprite("sweep")
+            battler:setAnimation("sweep")
             cutscene:text("* You pretend to clean the arena of all its dust!")
-            cutscene:text("* Dusteer feels happy your collecting parts of its... body...?")
             self:addMercy(50)
-            
+            battler:setAnimation("battle/idle")
+            cutscene:text("* Dusteer is flattered that you helped it be less messy!")
+            self:resetSprite()
         end)
     end 
     return super.onAct(self, battler, name)
 end
-function dusteer:onXAct(battler, name)
-    if name == "N-Sweep" then
-        Game.battle:startActCutscene(function(cutscene)
-            set.animation = "sweep"
-            local noelle = Game.battle:getPartyBattler("noelle")
-            local x, y = Game.battle.enemies[1].x, Game.battle.enemies[1].y
-            noelle:slideTo(x, y, 0.5)
-            noelle:setAnimation("sweep")
-            cutscene:text("* Noelle cleans the arena of its dust!")
-            cutscene:text("* Dusteer feels happy your collecting parts of its... body...?")
-            self:addMercy(25)
+
+-- function dusteer:onXAct(battler, name)
+--     if name == "N-Sweep" then
+--         Game.battle:startActCutscene(function(cutscene)
+--             set.animation = "sweep"
+--             local noelle = Game.battle:getPartyBattler("noelle")
+--             local x, y = Game.battle.enemies[1].x, Game.battle.enemies[1].y
+--             noelle:slideTo(x, y, 0.5)
+--             noelle:setAnimation("sweep")
+--             cutscene:text("* Noelle cleans the arena of its dust!")
+--             cutscene:text("* Dusteer feels happy your collecting parts of its... body...?")
+--             self:addMercy(25)
             
-        end)
-    end 
-    return super.onXAct(self, battler, name)
-end
+--         end)
+--     end 
+--     return super.onXAct(self, battler, name)
+-- end
 
-function peonie:onSpared(pacified)
-    local spawn_x, spawn_y = self:getRelativePos(self.width/2, self.height/2, Game.battle)
-    local line_x = spawn_x - 50
-
-    for i = 1, 8 do
-        local stacked_y = (spawn_y - 48) + ((i - 1) * 12)
-        local addon = MathUtils.round(MathUtils.random(1, 2))
-        local sprite = Sprite("effects/petal_" .. addon, line_x, stacked_y)
-        
-        sprite:setScale(2)
-        sprite:setOrigin(0.5)
-        sprite:setLayer(self.layer + 0.001)
-
-        sprite.start_x = line_x
-        sprite.wave_time = love.math.random() * 10
-        sprite.fall_progress = 0
-        sprite.fall_speed = 120    
-        sprite.sway_speed = 5.0    
-        sprite.sway_width = 15     
-
-        Game.battle:addChild(sprite)
-
-        Game.battle.timer:every(0, function()
-            if not sprite or not sprite.stage then return false end
-
-            sprite.wave_time = sprite.wave_time + (DT * sprite.sway_speed)
-            sprite.fall_progress = sprite.fall_progress + (DT * sprite.fall_speed)
-
-            sprite.x = sprite.start_x + (math.sin(sprite.wave_time) * sprite.sway_width)
-            sprite.y = stacked_y + sprite.fall_progress
-            sprite.rotation = math.cos(sprite.wave_time) * 0.4
-            sprite.alpha = sprite.alpha - (DT * 1.5)
-
-            if sprite.x < 100 then
-                sprite:remove()
-                return false
-            end
-        end)
-    end
-
-    return super.onSpared(self, pacified)
-end
-
-function peonie:onDefeat()
-    if Game:getFlag("geno") then 
-    self:statusMessage("msg", "lost", {1, 1, 1})
-    self:onDefeatFatal()
-    else 
-    return super.onDefeat(self)
-    end 
-end 
-
-
-
-return peonie
+return dusteer
--- mods/after_chill/scripts/data/actors/kris.lua
+++ mods/after_chill/scripts/data/actors/kris.lua
@@ -5,7 +5,11 @@ function actor:init()
     self.portrait_offset = {-20, -10}
     -- Table of sprite animations
     TableUtils.merge(self.animations, {
-        ["sweep"]        = {"battle/sweep", 1/4, true}
+        ["sweep"]        = {"sweep", 1/4, true}
+    }, false)
+
+    TableUtils.merge(self.offsets, {
+        ["sweep"] = {-6, 7}
     }, false)
 end
 
