1d9d97efe5ce4a4cba07340c8cfb9223ef524a16
fluffyboy <sekalisukarumah@gmail.com>
2026-06-01 19:01:21 +1000

    Update shadow.lua

--- mods/after_chill/scripts/battle/enemies/shadow.lua
+++ mods/after_chill/scripts/battle/enemies/shadow.lua
@@ -56,37 +56,37 @@ function shadow:onAdd(parent)
     super.onAdd(self, parent)
 end 
 
-function shadow:onHurt(damage, battler)
-    super.onHurt(self, damage, battler)
-    if self.health <= (self.max_health * 0.98) then 
-        Game.battle.battle_ui:endAttack()
-        Game.battle:startCutscene(function(cutscene)
-            local kris = Game.battle.party[1]
-            kris:setAnimation("battle/idle")
-            cutscene:wait(1.3)
-            cutscene:setSpeaker("shadow")
-            Game.world.music:fade(0, 0.5)
-            cutscene:wait(0.5)
-            cutscene:text("* Wow,[wait:5] putting up quite the fight,[wait:5] aren't you?")
-            cutscene:text("* After all, killing me won't do [shake:1]anything[shake:0].")
-            Assets.playSound("suslaugh")
-            cutscene:wait(1)
-            cutscene:text("* Kris.[wait:10][noskip][speed:0.7]\n* Your actions don't matter.[wait:5]\n* They're all futile.")
-            Assets.playSound("ui_spooky_action", 1, 1.7)
-            Game.battle.timer:tween(0.5, self, {scale_y = 0})
-            cutscene:wait(0.7)
-            self:setActor("shadow")
-            self:removeFX(self.fx1)
-            self:removeFX(self.fx2)
-            self:setAnimation("ball")
-            self.alpha = 0 
-            local sfx = Assets.playSound("appear")
-            self:fadeTo(1, sfx:getDuration())
-            self:setScale(1)
-            cutscene:wait(sfx:getDuration())
-          --  self:jumpTo(700, 200)
-        end)
-    end 
-end 
+-- function shadow:onHurt(damage, battler)
+--     super.onHurt(self, damage, battler)
+--     if self.health <= (self.max_health * 0.98) then 
+--         Game.battle.battle_ui:endAttack()
+--         Game.battle:startCutscene(function(cutscene)
+--             local kris = Game.battle.party[1]
+--             kris:setAnimation("battle/idle")
+--             cutscene:wait(1.3)
+--             cutscene:setSpeaker("shadow")
+--             Game.world.music:fade(0, 0.5)
+--             cutscene:wait(0.5)
+--             cutscene:text("* Wow,[wait:5] putting up quite the fight,[wait:5] aren't you?")
+--             cutscene:text("* After all, killing me won't do [shake:1]anything[shake:0].")
+--             Assets.playSound("suslaugh")
+--             cutscene:wait(1)
+--             cutscene:text("* Kris.[wait:10][noskip][speed:0.7]\n* Your actions don't matter.[wait:5]\n* They're all futile.")
+--             Assets.playSound("ui_spooky_action", 1, 1.7)
+--             Game.battle.timer:tween(0.5, self, {scale_y = 0})
+--             cutscene:wait(0.7)
+--             self:setActor("shadow")
+--             self:removeFX(self.fx1)
+--             self:removeFX(self.fx2)
+--             self:setAnimation("ball")
+--             self.alpha = 0 
+--             local sfx = Assets.playSound("appear")
+--             self:fadeTo(1, sfx:getDuration())
+--             self:setScale(1)
+--             cutscene:wait(sfx:getDuration())
+--           --  self:jumpTo(700, 200)
+--         end)
+--     end 
+-- end 
 
 return shadow
