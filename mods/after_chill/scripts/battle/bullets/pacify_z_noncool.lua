e8cc0f8707305dc7f02a70646e253cbb254952c0
fluffyboy <sekalisukarumah@gmail.com>
2026-07-09 08:01:53 +1000

    Revert "Update ralsei.lua"

--- mods/after_chill/scripts/battle/bullets/pacify_z_noncool.lua
+++ mods/after_chill/scripts/battle/bullets/pacify_z_noncool.lua
@@ -5,8 +5,10 @@ function SpareZ:init(x, y, dir, speed)
     super.init(self, x, y, "effects/spare/z")
     self.tiredness = 12
     self.remove_offscreen = false
-    self:setOrigin(0.5, 0.5)
+    self:setScale(0.02)
+    self.graphics.grow = 0.04
     self:fadeOutSpeedAndRemove(0.02)
+    self:addFX(ColorMaskFX({0.4, 0.6, 1.0}, 0.4))
     self.damage = 0
     self.tp = 0
     self.grow_x = 0.1
@@ -17,12 +19,6 @@ function SpareZ:init(x, y, dir, speed)
     self.physics.direction = math.rad(dir)
     self.physics.friction = 1
     self.base_speed = 0
-end
-function SpareZ:update()
-    self.scale_x = self.scale_x + self.grow_x * DTMULT
-    self.scale_y = self.scale_y + self.grow_y * DTMULT
-
-    super.update(self)
 end
 
 return SpareZ
