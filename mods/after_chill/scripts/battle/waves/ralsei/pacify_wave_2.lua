9bca8610eab951a0eba6a34b7b8bc261371ee776
fluffyboy <sekalisukarumah@gmail.com>
2026-07-12 08:22:27 +1000

    Update pacify_wave_2.lua

--- mods/after_chill/scripts/battle/waves/ralsei/pacify_wave_2.lua
+++ mods/after_chill/scripts/battle/waves/ralsei/pacify_wave_2.lua
@@ -2,7 +2,7 @@ local Basic, super = Class(Wave)
 
 function Basic:init()
     super.init(self)
-    self.time = 15.8
+    self.time = 12.5
 end 
 
 function Basic:onArenaEnter()
@@ -12,25 +12,26 @@ end
 
 function Basic:onStart()
     Game.battle.arena:setFire(true, true)
+    if Game.battle.soul then 
     self.timer:everyInstant(1.8, function()
         local ralsei = self:getAttackers()[1]
-
         ralsei:setAnimation("spell", function()
         Assets.playSound("alert")
         local pacify_x, pacify_y = Game.battle.soul:getRelativePos(Game.battle.soul.width/2, Game.battle.soul.height/2, Game.battle)
         self.timer:after(0.13, function()       
         Assets.stopAndPlaySound("spell_pacify")
         local z_count = 0
-        self.timer:every(1/20, function ()
+        self.timer:every(1/20, function()
             z_count = z_count + 1
             local z = self:spawnBullet("pacify_z_noncool", pacify_x, pacify_y, z_count * -40)
-            z:setHitbox(2, 2, 18, 18)
+            z:setHitbox(4, 4, 14, 14)
             z.layer = Game.battle.soul.layer + 0.002
             z.tiredness = 24 
         end, 8)
     end) 
 end)
 end)
+end 
 end
 
 
