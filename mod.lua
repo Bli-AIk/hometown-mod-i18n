8fc806db78199f955bd67e322306bc401d22290f
Bli_Aik <haikun2333@126.com>
2026-08-08 15:57:19 +0800

    Slim fork: keep hometown_pack subtree only (BSD-3 LICENSE retained)

---
+++ mod.lua
@@ -0,0 +1,61 @@
+function Mod:init()
+    self:setMusicVolumes()
+end
+
+function Mod:postInit(newfile)
+    print("Loaded "..self.info.name.."!")
+end
+
+function Mod:setMusicVolumes()
+    MUSIC_VOLUMES["deltarune/noelle_house_wip"] = 0.9
+    MUSIC_VOLUMES["deltarune/noelle_distant"] = 0.8
+end
+
+function Mod:onMapMusic(map, music)
+	if music == "hometown" then
+		if Game:getFlag("hometown_time", "day") == "day" then
+			return "deltarune/town_day"
+		elseif Game:getFlag("hometown_time", "day") == "sunset" then
+			return "deltarune/town"
+		elseif Game:getFlag("hometown_time", "day") == "night" then
+			return "forecasted_hometown_night"
+		end
+	end
+	if music == "church" then
+		if Game:getFlag("hometown_time", "day") == "night" then
+			return "deltarune/church_lw_night"
+		else
+			return "deltarune/church_lw"
+		end
+	end	
+	if music == "deltarune/mus_school" then
+		if Game:getFlag("hometown_time", "day") == "sunset" then
+			return "deltarune/mus_birdnoise"
+		elseif Game:getFlag("hometown_time", "day") == "night" then
+			return "deltarune/night_ambience"
+		else
+			return "deltarune/mus_school"
+		end
+	end
+	if music == "deltarune/mus_birdnoise" and Game:getFlag("hometown_time", "day") == "night" then
+		return "deltarune/night_ambience"
+	end
+end
+
+function Mod:onMapBorder(map, border)
+	if border == "leaves" and Game:getFlag("hometown_time", "day") == "night" then
+		return "leaves_night"
+	end
+end
+
+function Mod:loadObject(world, name, data)
+    if data.gid then
+		local tobj = world.map:createTileObject(data)
+		tobj.day_mode = data.properties["day"] or nil
+		tobj.night_mode = data.properties["night"] or nil
+		tobj.sunset_mode = data.properties["sunset"] or nil
+		tobj.sunrise_mode = data.properties["sunrise"] or nil
+		tobj.rain_mode = data.properties["rain"] or nil
+		return tobj
+    end
+end
